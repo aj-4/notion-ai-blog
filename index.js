d454ec2ba1e2206e317aa0ffecc399573d0e72b3
Aaron Jack <apietsch4@gmail.com>
2024-10-14 15:00:57 +0900

    rm express

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
 const { Client } = require("@notionhq/client");
 const axios = require('axios');
 require('dotenv').config();
